3f8fd316761ef52f4975e2ac773b7db121d55d42
Shankar Raman <shankaraman.r@gmail.com>
2014-03-18 22:09:42 +0530

    Finished Paper Client Assignment and made changes

--- Distributed-Sytems/Paper_Server-XML-RPC/client.py
+++ Distributed-Sytems/Paper_Server-XML-RPC/client.py
@@ -9,6 +9,8 @@ def commands():
 def core():
     input_option = input("Enter your choice:")
     server = xmlrpclib.Server("http://localhost:8000")
+
+    # Loopback!
     if input_option == 2:
         print server.list()
         status = raw_input("Still do you want to continue? Y or N:")
@@ -16,18 +18,45 @@ def core():
             commands()
             core()
         else:
-            pass
+            sys.exit()
+
+    # Adding a paper to server
     if input_option == 1:
         author_name = raw_input("Enter the author name:")
         paper_title = raw_input("Enter the Paper Title:")
-        with open(sys.argv[1],"rb") as handle:
+        file_name = raw_input("Enter the name of the file:")
+        with open(file_name,"rb") as handle:
             binary_data = xmlrpclib.Binary(handle.read())
-        server.server_receive_file(binary_data,author_name,paper_title)
+        server.server_receive_file(binary_data,author_name,paper_title,file_name)
         status = raw_input("Still do you want to continue? Y or N:")
         if status == "Y" or status == "y":
             commands()  
             core()
         else:
-            pass
+            sys.exit()
+
+    # Fetching information about a paper
+    if input_option == 3:
+        uid = raw_input("Enter the unique id of the paper:")
+        print server.paper_send_information(uid)
+        status = raw_input("Still do you want to continue? Y or N:")
+        if status == "Y" or status == "y":
+            commands()
+            core()
+        else:
+            sys.exit()
+
+    # Displays the contents of a file
+    if input_option == 4:
+        uid = raw_input("Enter the unique id of the paper:")
+        print server.print_file_contents(uid)
+        status = raw_input("Still do you want to continue? Y or N:")
+        if status == "Y" or status == "y":
+            commands()
+            core()
+        else:
+            sys.exit()
+
+# Main function
 commands()
 core()
--- Distributed-Sytems/Paper_Server-XML-RPC/server.py
+++ Distributed-Sytems/Paper_Server-XML-RPC/server.py
@@ -1,19 +1,75 @@
 import SimpleXMLRPCServer
 import os,sys
-import hashlib
+import hashlib,random
+
+
+# Removes the directory named files and creates a new directory named 'files'
+
+os.system("rm -r files")
+os.mkdir("files")
+
+# Global Variables
+
+session_list = []
+dictionary = {}
+count = 0 # Whether to update or add values to dictionary
 
 class PaperServer:
 
+"""    
+    Lists the contents inside the "files" folder
+    Returns the unique string with author name and title of a paper
+"""
     def list(self):
+        global count,dictionary,session_list
         output = os.popen("ls files/").readlines()
         data = ' '.join(output)
-        return data
+        return str("Author:"+dictionary.values()[0][0]+"\nTitle"+dictionary.values()[0][1]+"\nUnique id:"+str(dictionary.keys()).strip("[]''"))
 
-    def server_receive_file(self,arg,author_name,paper_title):
+"""
+    # Stores the file under the "files" folder
+    # Stores the author name and title
+    # Generates a unique string for a given file
+"""
+
+    def server_receive_file(self,arg,author_name,paper_title,file_name):
+        global count,dictionary,session_list
+        m = hashlib.md5()
+        lst = []
+        count+=1
         with open("files/"+str(paper_title),"wb") as handle:
             handle.write(arg.data)
-            return True
+        uid = m.update(arg.data)
+        uid = m.hexdigest()
+        if count == 1:
+            session_list.append(author_name)
+            session_list.append(paper_title)
+            session_list.append(file_name)
+            dictionary = {uid:session_list}
+        else:
+            session_list = lst
+            dictionary.update({uid:session_list})
+        return True
 
+    """
+        For a given unique string, it returns the author name
+        and the title of a paper
+    """
+
+    def paper_send_information(self,uid):
+        global dictionary
+        return str("Author Name: "+dictionary[uid][0]+"\nTitle: "+dictionary[uid][1])
+
+    """
+        Fetches the file from the server and it prints the
+        contents
+    """
+
+    def print_file_contents(self,uid):
+        fd = open(dictionary.values()[0][2],"rb")
+        data = fd.readlines()
+        return str(data).strip("[]")
+    
 server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost",8000))
 server.register_instance(PaperServer())
 server.serve_forever()
