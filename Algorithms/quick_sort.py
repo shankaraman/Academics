dd887f439a55609bd895069c695795d147222c16
shankaraman <shankaraman.r@gmail.com>
2014-09-14 14:23:44 +0530

    quick sort

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -1,30 +1,30 @@
 class Quicksort:
     def Partition(self,array,start,end):
         # Assigning last element of the array as pivot
-        pivot,pIndex = array[end],array[start]
-        i = start
+        pivot,pIndex,i = array[end],start,start
+#        print pivot
         while i < end:
+            print i,"<",end
             if array[i] <= pivot:
+                print array[i],"<=",pivot
+                print "i:",i,"pIndex:",pIndex
+#                print "Before swapping:",array[i],array[pIndex]
                 array[i],array[pIndex] = array[pIndex],array[i]
+#                print "After swapping:",array[i],array[pIndex]
                 pIndex+=1
             i+=1
-        if pIndex != len(array):
             array[pIndex],array[end] = array[end],array[pIndex]
-            print pInde
             return pIndex
 
     def Quick(self,array,start,end):
-        print start,end
         if start < end:
             pIndex = self.Partition(array,start,end)
             print pIndex
-            # Left of Pivot
-            Quick(array,start,pIndex-1)
-            # Right of Pivot
-            Quick(array,pIndex+1,end)
-        print array
+            self.Quick(array,start,pIndex-1)
+            self.Quick(array,pIndex+1,end)
+            print array
 
 # Main
 obj = Quicksort()
-lst = [9,7,5,55,1,2,4,5,33,66,6,2,22]
+lst = [7,2,1,6,8,5,3,4]
 obj.Quick(lst,0,len(lst)-1)
